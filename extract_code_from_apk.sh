b444c422fbffc5fbe3295ffa40b2e6687c40f02f
Kevin Campusano <megakevinx@gmail.com>
2015-02-16 19:18:16 -0500

    Added command for obtaining the callgraph of a given .jar. Added a list of the needed tools.

--- extract_code_from_apk.sh
+++ extract_code_from_apk.sh
@@ -1,20 +1,27 @@
+## Need to have these:
+## openjdk-7-jdk
+## apktool
+## dex2jar
+
 
 ## Start analyzing the apk files
-java -jar apktool1.5.2/apktool.jar d -f input/com.metago.astro.apk output/
+java -jar apktool1.5.2/apktool.jar d -f input/com.facebook.orca.apk output/
 
 ## Create the dex file
-jar xvf input/com.metago.astro.apk classes.dex
+jar xvf input/com.zaneashby.ircshare.apk classes.dex
 mv classes.dex output
 
+## Create the jar based on the dex file
 dex2jar-0.0.9.15/dex2jar.sh output/classes.dex
 
-## Switching locations was the only way to have everything output in the appropriate location.
+## Extract the contents of the jar
 cd output
 jar xvf classes_dex2jar.jar 
 cd ..
 
-### An ugly hack
-cd ../../
-
 ## Now convert all of the .class files to .java
 java -jar jd-cmd/jd-cli/target/jd-cli.jar output/ -od output/
+
+## Obtain call graph from jar
+java -jar /home/kevin/Desktop/java-callgraph/target/javacg-0.1-SNAPSHOT-static.jar output/classes_dex2jar.jar 
+
