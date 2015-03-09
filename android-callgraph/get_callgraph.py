1cf1fdabeb063be3b8f37c1f982c3b2a623c1750
Kevin Campusano <megakevinx@gmail.com>
2015-03-08 21:26:41 -0400

    Created python script to generate the call graph of a given apk.

---
+++ android-callgraph/get_callgraph.py
@@ -0,0 +1,44 @@
+__author__ = 'kevin'
+
+import argparse
+import subprocess
+
+
+def main():
+
+    args = parse_args()
+
+    # jar xvf [APK FILE] classes.dex
+    # dex2jar-0.0.9.15/dex2jar.sh classes.dex
+    # java -jar javacg-0.1-SNAPSHOT-static.jar output/classes_dex2jar.jar > javacg.txt
+
+    # Create the dex file from the APK
+    subprocess.call(["jar", "xvf", "input/" + args.apk, "classes.dex"])
+
+    # Move the dex file into the output folder
+    subprocess.call(["mv", "classes.dex", "output/" + args.apk + ".dex"])
+
+    # Create the jar from dex file
+    subprocess.call(["dex2jar-0.0.9.15/dex2jar.sh", "output/" + args.apk + ".dex"])
+
+    # Obtain call graph from jar
+    subprocess.call(["java", "-jar", "java-callgraph/target/javacg-0.1-SNAPSHOT-static.jar",
+                     "output/" + args.apk + "_dex2jar.jar"],
+                    stdout=open("output/" + args.apk + ".cg.txt", "w"))
+
+    subprocess.call(["java", "-jar", "java-callgraph/target/javacg-0.1-SNAPSHOT-static.jar", "output/demo.apk_dex2jar.jar"], stdout=open("output/demo.apk.cg.txt", "w"))
+
+
+def parse_args():
+
+    parser = argparse.ArgumentParser(
+        description="Analyzes a software's source code and reports various metrics related to it's attack surface.")
+
+    parser.add_argument("-apk", "--apk",
+                        help="The apk of the application from which to extract the call graph.")
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
