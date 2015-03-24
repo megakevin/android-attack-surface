23336d7357502b68ea58d555c32226c94493cfc2
Kevin Campusano <megakevinx@gmail.com>
2015-03-24 13:01:59 -0400

    Refactored the get_reviews.py script. Looks much better now. App id and number of pages are supplied as arguments through CLI now.

--- googleplay-scraper/get_reviews.py
+++ googleplay-scraper/get_reviews.py
@@ -4,79 +4,129 @@ import urllib.request
 import urllib.parse
 import json
 import re
-import xml.etree.ElementTree as ET
 from xml.dom import minidom
-import csv
+import time
+import sqlite3
+import argparse
 
 
 def main():
+    args = parse_args()
 
-    max_pages = 10
-
-    csv_file = open("reviews.csv", "a", newline='')
-    csv_writer = csv.writer(csv_file)
-
-    raw_file = open("raw.html", "a", newline='')
+    max_pages = args.pages
 
     for page_number in range(1, max_pages):
 
-        request = urllib.request.Request("https://play.google.com/store/getreviews", method="POST")
+        print("Sleeping...")
+        time.sleep(5)
 
-        request.add_header("content-type", "application/x-www-form-urlencoded; charset=utf-8")
-        request.add_header("origin", "https://play.google.com")
-        request.add_header("cache-control", "no-cache")
-        # request.add_header("referer", "https://play.google.com/store/apps/details?id=com.github.mobile&hl=en")
+        response = make_request(args.application, page_number)
+        print("Request made")
 
-        request.add_header("referer", "https://play.google.com/store")
-        request.add_header("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36")
+        reviews = parse_response(response)
+        print("Response parsed")
 
-        request.add_header("x-client-data", "CJW2yQEIpbbJAQiptskBCJ6SygEIrZXKARiricoB")
-        request.add_header("x-requested-with", "XMLHttpRequest")
+        save_reviews(args.application, reviews)
+        print("Reviews saved into the database")
 
-        data = urllib.parse.urlencode({'reviewType': "0",
-                                       'pageNum': 1,
-                                       'id': "com.github.mobile",
-                                       'reviewSortOrder': "0",
-                                       'xhr': "1",
-                                       'token': "oCu4PNapSrAMxh2-vqD_iMxod_g:1394178244711"})
-        data = data.encode('utf-8')
 
-        response = urllib.request.urlopen(request, data)
+def parse_args():
+    """
+        Provides a command line interface.
 
-        # this is to turn the bytes that come in the response into a python string
-        response = str(response.read(), "utf-8")
+        Defines all the positional and optional arguments along with their respective valid values
+        for the command line interface and returns all the received arguments as an object.
 
-        # apparently random garbage
-        response = response.replace(")]}'", " ")
+        Returns:
+            An object that contains all the provided command line arguments.
+    """
+    parser = argparse.ArgumentParser(
+        description="Gets all the user reviews on the specified APK and saves it into a database.")
 
-        # this converts the string to json and takes care of all unescaping
-        response = json.loads(response)
+    parser.add_argument("-app", "--application",
+                        help="The id of the app to get the reviews from.")
 
-        reviews = response[0][2]
+    parser.add_argument("-p", "--pages", type=int,
+                        help="The number of review pages to request.")
 
-        # deleting all <img> tags because since they don't close, they are not valid xml
-        while re.search(r"<img\s[^>]*?src\s*=\s*['\"]([^'\"]*?)['\"][^>]*?>", reviews):
-            m = re.search(r"<img\s[^>]*?src\s*=\s*['\"]([^'\"]*?)['\"][^>]*?>", reviews)
-            reviews = reviews.replace(m.group(0), "")
+    return parser.parse_args()
 
-        raw_file.write(reviews)
 
-        xmldoc = minidom.parseString("<reviews>" + reviews + "</reviews>")
+def make_request(application_id, page_number):
+    request = urllib.request.Request("https://play.google.com/store/getreviews", method="POST")
 
-        divs = xmldoc.getElementsByTagName("div")
-        for div in divs:
-            if div.getAttribute('class') == "review-body":
+    request.add_header("content-type", "application/x-www-form-urlencoded; charset=utf-8")
+    request.add_header("origin", "https://play.google.com")
+    request.add_header("cache-control", "no-cache")
+    # request.add_header("referer", "https://play.google.com/store/apps/details?id=com.github.mobile&hl=en")
 
-                review_title = ""
-                if div.childNodes[1].firstChild:
-                    review_title = div.childNodes[1].firstChild.nodeValue
+    request.add_header("referer", "https://play.google.com/store")
+    request.add_header("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36")
 
-                review_body = div.childNodes[2].nodeValue
+    request.add_header("x-client-data", "CJW2yQEIpbbJAQiptskBCJ6SygEIrZXKARiricoB")
+    request.add_header("x-requested-with", "XMLHttpRequest")
 
-                # TODO: Write to database instead
-                csv_writer.writerow([review_title, review_body])
+    data = urllib.parse.urlencode({'reviewType': "0",
+                                   'pageNum': page_number,
+                                   'id': application_id,  # "com.github.mobile"
+                                   'reviewSortOrder': "0",
+                                   'xhr': "1",
+                                   'token': "oCu4PNapSrAMxh2-vqD_iMxod_g:1394178244711"})
+    data = data.encode('utf-8')
 
-                print(review_title + ">" + review_body)
+    response = urllib.request.urlopen(request, data)
+
+    return response
+
+
+def parse_response(response):
+    result = []
+
+    # this is to turn the bytes that come in the response into a python string
+    response = str(response.read(), "utf-8")
+
+    # apparently random garbage
+    response = response.replace(")]}'", " ")
+
+    # this converts the string to json and takes care of all unescaping
+    response = json.loads(response)
+
+    # this is where the reviews are
+    reviews = response[0][2]
+
+    # deleting all <img> tags because since they don't close, they are not valid xml
+    while re.search(r"<img\s[^>]*?src\s*=\s*['\"]([^'\"]*?)['\"][^>]*?>", reviews):
+        m = re.search(r"<img\s[^>]*?src\s*=\s*['\"]([^'\"]*?)['\"][^>]*?>", reviews)
+        reviews = reviews.replace(m.group(0), "")
+
+    xmldoc = minidom.parseString("<reviews>" + reviews + "</reviews>")
+
+    divs = xmldoc.getElementsByTagName("div")
+    for div in divs:
+        if div.getAttribute('class') == "review-body":
+
+            review_title = ""
+            if div.childNodes[1].firstChild:
+                review_title = div.childNodes[1].firstChild.nodeValue
+
+            review_body = div.childNodes[2].nodeValue
+
+            result.append({
+                'title': review_title,
+                'body': review_body
+            })
+
+    return result
+
+
+def save_reviews(application_id, reviews):
+
+    reviews_to_insert = [(application_id, r['title'], r['body']) for r in reviews]
+
+    with sqlite3.connect('reviews.db') as db:
+        db.executemany('INSERT INTO reviews ('
+                       'app, title, body) '
+                       'VALUES (?, ?, ?)', reviews_to_insert)
 
 
 if __name__ == '__main__':
