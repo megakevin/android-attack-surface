37f94bc12f5e4043ede7d5d6bbe20a3c18b3e43e
Kevin Campusano <megakevinx@gmail.com>
2015-04-05 23:11:34 -0400

    Added copy of google play api source to the new apk-download path.

---
+++ attack-surface/apk-download/googleplay/config.py
@@ -0,0 +1,13 @@
+#separator used by search.py, categories.py, ...
+SEPARATOR = ';'
+
+# Visit https://github.com/egirault/googleplay-api for instructions on how to configure this file
+LANG            = 'en_US' # can be en_US, fr_FR, ...
+ANDROID_ID      = '3160CF56C7E077B0'
+GOOGLE_LOGIN    = "androidattacksurface@gmail.com"
+GOOGLE_PASSWORD = "androidattacksurface123456"
+AUTH_TOKEN      = None
+
+# force the user to edit this file
+# if any([each == None for each in [ANDROID_ID, GOOGLE_LOGIN, GOOGLE_PASSWORD]]):
+#     raise Exception('config.py not updated')
