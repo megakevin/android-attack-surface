088ff7bd0efa86fb70a0978bb06c8c751cba4b82
Kevin Campusano <megakevinx@gmail.com>
2015-04-20 01:03:28 -0400

    Added query that gets the edge blacklist in a form similar to java-callgraph output so that it can be used by attacksurfacemeter's JavaCGLoader.

--- callgraph-database/select_package_black_list.sql
+++ callgraph-database/select_package_black_list.sql
@@ -10,3 +10,13 @@ with packages (package_name, sum_quantity) as (
     select callee_package, sum(quantity) from repeated_edges group by callee_package
 )
 select package_name, sum(sum_quantity) from packages group by package_name;
+
+-- edge black list
+select caller, callee, 'M:' || caller || ' (O)' || callee, count(*)
+from edges
+group by caller, callee, 'M:' || caller || ' (O)' || callee
+--order by count(*) desc
+having count(*) > 20;
+
+with apps (app) as (select app from edges group by app)
+select count(*) from apps;
