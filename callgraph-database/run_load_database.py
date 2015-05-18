4aa35a2041b512b8ec40c6c96d8d706f86c99efb
Kevin Campusano <megakevinx@gmail.com>
2015-05-18 13:17:57 -0400

    Added script to create a call graph database with all downloaded apps. This was to develop a more comprehensive edge black list but was not used due to time constraints.

---
+++ callgraph-database/run_load_database.py
@@ -0,0 +1,71 @@
+__author__ = 'kevin'
+
+import argparse
+import os
+import psycopg2
+from connection import PostgreSQL
+
+from load_database import load_database
+
+
+def main():
+    args = parse_args()
+    callgraph_files_location = args.path_to_callgraph_files
+    callgraph_file_extension = '.apk.cg.txt'
+
+    apps_info = get_apks_info()
+
+    for app in apps_info:
+        callgraph_file = os.path.join(callgraph_files_location, app['apk_name'] + callgraph_file_extension)
+
+        if os.path.exists(callgraph_file) and os.path.getsize(callgraph_file) < 10000000 and os.path.getsize(callgraph_file) > 0:
+            print("Loading database with edges from: " + app['apk_name'])
+            load_database(callgraph_file, app['apk_name'])
+
+        else:
+            print("Skipping: " + app['apk_name'])
+
+
+def parse_args():
+    """
+        Provides a command line interface.
+
+        Defines all the positional and optional arguments along with their respective valid values
+        for the command line interface and returns all the received arguments as an object.
+
+        Returns:
+            An object that contains all the provided command line arguments.
+    """
+    parser = argparse.ArgumentParser(
+        description="Downloads the specified APK from the google play store.")
+
+    parser.add_argument("path_to_callgraph_files",
+                        help="The path where the call graph files generated using java-callgraph are located.")
+
+    return parser.parse_args()
+
+
+def get_apks_info():
+    apkinfo_select_stmt = '''SELECT id, apkname
+                             FROM apkinformation
+                             WHERE isdownloaded = TRUE
+                             AND lowerdownloads > 1000;'''
+
+    db = psycopg2.connect(PostgreSQL.connection_string)
+    c = db.cursor()
+
+    c.execute(apkinfo_select_stmt)
+    apps = c.fetchall()
+
+    apps = [{'id': a[0],
+             'apk_name': a[1]}
+            for a in apps]
+
+    c.close()
+    db.close()
+
+    return apps
+
+
+if __name__ == '__main__':
+    main()
